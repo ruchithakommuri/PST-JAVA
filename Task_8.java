0245f660448f0d32330840b15ed046403ca23ce4
ruchithakommuri <ruchithakommuri3@gmail.com>
2026-07-26 15:41:15 +0530

    task_8java

---
+++ Task_8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        if (n == 0) {
+            System.out.println(a);
+        } else if (n == 1) {
+            System.out.println(b);
+        } else {
+            int c = 0;
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.println(c);
+        }
+    }
+}
