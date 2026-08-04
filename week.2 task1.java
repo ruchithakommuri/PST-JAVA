fc41ea14c77a918b9fab12495938d39ac84a3c5f
ruchithakommuri <ruchithakommuri3@gmail.com>
2026-08-04 17:49:31 +0530

    week.2task1.java

---
+++ week.2 task1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Task1.java {
+    public static int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        System.out.println(Arrays.toString(buildArray(nums)));
+    }
+}
