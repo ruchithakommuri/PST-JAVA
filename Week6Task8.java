ef5ce3a537ced1f3aaa850e5fa8f85df991ffe25
ruchithakommuri <ruchithakommuri3@gmail.com>
2026-08-20 11:01:58 +0530

    Create Week6Task8.java

---
+++ Week6Task8.java
@@ -0,0 +1,31 @@
+public class Week6Task8 {
+
+    static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            int j = 0;
+
+            while (j < needle.length() &&
+                   haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+
+        System.out.println(strStr(haystack, needle));
+    }
+}
