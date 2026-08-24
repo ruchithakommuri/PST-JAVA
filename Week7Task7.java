9ee75c9dc4be6909bbb0d2e0e1c4041fe1ccbdba
ruchithakommuri <ruchithakommuri3@gmail.com>
2026-08-24 11:36:40 +0530

    Create Week7Task7.java

---
+++ Week7Task7.java
@@ -0,0 +1,26 @@
+public class Week7Task7 {
+
+    static class Animal {
+        void walk() {
+            System.out.println("I am walking");
+        }
+    }
+
+    static class Bird extends Animal {
+        void fly() {
+            System.out.println("I am flying");
+        }
+
+        void sing() {
+            System.out.println("I am singing");
+        }
+    }
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
